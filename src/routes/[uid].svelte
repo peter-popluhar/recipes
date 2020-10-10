7ef25ecdd4beb469fbd5cbe57bebd572d818d196
Peter Popluhar <popluhar.peter@gmail.com>
2020-10-10 23:50:49 +0200

    fetch data from Prismic headless cms

---
+++ src/routes/[uid].svelte
@@ -0,0 +1,30 @@
+<script context="module">
+  import PrismicDOM from "prismic-dom";
+  import { Client, linkResolver } from "../../prismic-config";
+
+  let card = null;
+
+  export async function preload({ params, query }) {
+    const uid = params.uid;
+    card = await Client.getByUID("card", uid);
+
+    if (card) {
+      return { card };
+    } else {
+      this.error(res.status, data.message);
+    }
+  }
+</script>
+
+<script>
+
+  export let card;
+  const {image, title, source } = card.data;
+  console.log(card.data)
+</script>
+
+<p>{PrismicDOM.RichText.asText(title)}</p>
+Originall Source: <a href="{source.url}" target="_blank">here</a>
+<img src="{image.url}" alt="">
+<p>Ingredients:</p>
+

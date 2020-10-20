3440fe840957888dda526237e16fa50ee0a8f4cf
Peter Popluhar <popluhar.peter@gmail.com>
2020-10-21 00:17:30 +0200

    display data from Prismic slice components

--- src/routes/[uid].svelte
+++ src/routes/[uid].svelte
@@ -19,12 +19,51 @@
 <script>
 
   export let card;
-  const {image, title, source } = card.data;
-  console.log(card.data)
+  export let ingredientsSlices = [];
+  export let instructions = [];
+
+  const {image, title, source, category, body } = card.data;
+
+  body.map(item => {
+    switch (item.slice_type) {
+      case 'ingredients': ingredientsSlices.push(item)
+        break;
+      case 'instructions': instructions.push(item)
+    }
+  })
+
+  // console.log(instructions)
+  
 </script>
 
 <p>{PrismicDOM.RichText.asText(title)}</p>
-Originall Source: <a href="{source.url}" target="_blank">here</a>
+<p>in category:{category}</p>
+<p> Originall Source: <a href="{source.url}" target="_blank">here</a></p>
 <img src="{image.url}" alt="test">
-<p>Ingredients:</p>
 
+<p>Ingredients:</p>
+<ul>
+  {#each ingredientsSlices as slice}
+  <li>
+    {PrismicDOM.RichText.asText(slice.primary.title1)}
+    <ul>
+      {#each slice.primary.items as {text}}
+        <li>
+          {text}
+        </li>
+      {/each}
+    </ul>
+  </li>
+  {/each}
+</ul>
+
+<p>instructions:</p>
+<ul>
+  {#each instructions as instruction}
+      {#each instruction.primary.instructions as {text}}
+        <li>
+          {text}
+        </li>
+      {/each}
+  {/each}
+</ul>

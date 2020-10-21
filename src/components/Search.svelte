3c642d7d9de4be1ec5b3e8e32aecbbee1943a987
Peter Popluhar <peter.popluhar@socialbakers.com>
2020-10-21 18:09:41 +0200

    Search Bar init

---
+++ src/components/Search.svelte
@@ -0,0 +1,26 @@
+<script>
+	import PrismicDOM from "prismic-dom";
+	export let data = [];
+	export let filtered;
+
+	let input = undefined;
+	let value = ""
+
+	$: filtered = data.filter(item =>
+			item.data.title[0].text.toLowerCase().includes(value.toLowerCase())
+	);
+
+
+</script>
+
+<input bind:this="{input}" type="search" bind:value/>
+
+<ul>
+	{#each filtered as item}
+		<li>
+			<a rel="prefetch" href="{item.uid}">
+				{PrismicDOM.RichText.asText(item.data.title)}
+			</a>
+		</li>
+	{/each}
+</ul>
